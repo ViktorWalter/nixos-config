de7bf838f655d756c561dbe4865ed576488dd88c
Viktor Walter <viktor.walter@disroot.org>
2026-07-21 20:39:39 +0200

    More configuration

---
+++ home-manager/neofetch/neofetch.nix
@@ -0,0 +1,18 @@
+programs.hyfetch = { #maintained neofetch fork
+  enable = true;
+  settings = {
+    preset: "leather",
+    mode: "rgb",
+    auto_detect_light_dark: false,
+    light_dark: "dark",
+    lightness: 0.56,
+    color_align: {
+        mode: "custom",
+        custom_colors: {
+            1: 1,
+            2: 2
+        }
+    },
+    backend: "neofetch",
+  };
+};
