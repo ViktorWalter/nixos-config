1b53ee482bac7f46e07c33b0966b78efc0ecfaa7
Viktor Walter <viktor.walter@disroot.org>
2026-07-21 21:09:35 +0200

    Fixed hyfetch config

--- home-manager/neofetch/neofetch.nix
+++ home-manager/neofetch/neofetch.nix
@@ -3,19 +3,20 @@
   programs.hyfetch = { #maintained neofetch fork
     enable = true;
     settings = {
-      preset: "leather",
-      mode: "rgb",
-      auto_detect_light_dark: false,
-      light_dark: "dark",
-      lightness: 0.56,
-      color_align: {
-          mode: "custom",
-          custom_colors: {
-              1: 1,
-              2: 2
-          }
-      },
-      backend: "neofetch",
+      preset = "leather";
+      mode = "rgb";
+      auto_detect_light_dark = false;
+      light_dark = "dark";
+      lightness = 0.56;
+      color_align = {
+          mode = "custom";
+          custom_colors = {
+              "1" = 1;
+              "2" = 2;
+          };
+      };
+      pride_month_disable = true; #some animation
+      backend = "neofetch";
     };
   };
 }
