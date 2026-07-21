ae5b41b3a3242ab107e687b49046f389d356ae99
Viktor Walter <viktor.walter@disroot.org>
2026-07-21 20:41:13 +0200

    Fix for hyfetch config

--- home-manager/neofetch/neofetch.nix
+++ home-manager/neofetch/neofetch.nix
@@ -1,18 +1,21 @@
-programs.hyfetch = { #maintained neofetch fork
-  enable = true;
-  settings = {
-    preset: "leather",
-    mode: "rgb",
-    auto_detect_light_dark: false,
-    light_dark: "dark",
-    lightness: 0.56,
-    color_align: {
-        mode: "custom",
-        custom_colors: {
-            1: 1,
-            2: 2
-        }
-    },
-    backend: "neofetch",
+{ hostName, athame-flake, config, pkgs, lib, ... }:
+{
+  programs.hyfetch = { #maintained neofetch fork
+    enable = true;
+    settings = {
+      preset: "leather",
+      mode: "rgb",
+      auto_detect_light_dark: false,
+      light_dark: "dark",
+      lightness: 0.56,
+      color_align: {
+          mode: "custom",
+          custom_colors: {
+              1: 1,
+              2: 2
+          }
+      },
+      backend: "neofetch",
+    };
   };
-};
+}
